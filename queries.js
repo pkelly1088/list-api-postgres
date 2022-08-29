9dff6b5f0dd0723bf56d51953f65c8f02afb4f16
Patrick Kelly <PatrickKelly@Meghans-MacBook-Air.local>
2022-08-29 19:40:05 -0400

    allow cors in headers

--- queries.js
+++ queries.js
@@ -1,17 +1,22 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'shoppers',
-  password: 'password',
+  user: PROCESS.ENV.PG_USER,
+  host: PROCESS.ENV.PG_HOST,
+  database: PROCESS.ENV.PG_DATABASE,
+  password: PROCESS.ENV.PG_PASSWORD,
   port: 5432,
 })
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM people;', (error, results) => {
-      if (error) {
-        throw error
-      }
+    pool.query(`SELECT * FROM ${PROCESS.ENV.PG_TABLE};`, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.header("Access-Control-Allow-Origin", "*")
+        response.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested, Content-Type, Accept Authorization"
+        )
       response.status(200).json(results.rows)
     })
   }
