27727802a75f4b05bd79aa291590841dd8cfb803
Patrick Kelly <PatrickKelly@Meghans-MacBook-Air.local>
2022-08-29 19:50:31 -0400

    revise server to work with cors

--- queries.js
+++ queries.js
@@ -1,22 +1,29 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: PROCESS.ENV.PG_USER,
-  host: PROCESS.ENV.PG_HOST,
-  database: PROCESS.ENV.PG_DATABASE,
-  password: PROCESS.ENV.PG_PASSWORD,
+  user: 'me',
+  host: 'localhost',
+  database: 'shoppers',
+  password: 'password',
   port: 5432,
 })
 
 const getUsers = (request, response) => {
-    pool.query(`SELECT * FROM ${PROCESS.ENV.PG_TABLE};`, (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.header("Access-Control-Allow-Origin", "*")
+    pool.query('SELECT * FROM people;', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.header("Access-Control-Allow-Origin", "*")
+      response.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (request.method === "OPTIONS") {
         response.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested, Content-Type, Accept Authorization"
-        )
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+    )
+    return response.status(200).json(results.rows)
+  }
       response.status(200).json(results.rows)
     })
   }
