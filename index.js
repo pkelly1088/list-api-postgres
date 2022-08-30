564099882d7837737ff6dc91478a91d48cdaa953
Patrick Kelly <PatrickKelly@Meghans-Air.home>
2022-08-30 15:11:41 -0400

    revise server

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(
   })
 );
 
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (request.method === "OPTIONS") {
+    response.header(
+    "Access-Control-Allow-Methods",
+    "POST, PUT, PATCH, GET, DELETE"
+    )
+  }
+  next();
+});
+
 app.get('/people', db.getUsers);
 
 app.listen(port, () => {
--- queries.js
+++ queries.js
@@ -15,18 +15,6 @@ const getUsers = (request, response) => {
       if (error) {
         throw error
       }
-      response.setHeader("Access-Control-Allow-Origin", "*")
-      response.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (request.method === "OPTIONS") {
-        response.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-    )
-    return response.status(200).json(results.rows)
-  }
       response.status(200).json(results.rows)
     })
   }
