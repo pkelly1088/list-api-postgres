6ce7ca9ad3eab2a5b5a3a570071a8cb473ba1cc2
Patrick Kelly <PatrickKelly@Meghans-Air.home>
2022-08-30 16:59:21 -0400

    revise api and queries

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 const db = require('./queries');
 const port = process.env.PG_PORT || 3001;
 
-app.use(morgan("dev"));
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  if (request.method === "OPTIONS") {
-    response.header(
-    "Access-Control-Allow-Methods",
-    "POST, PUT, PATCH, GET, DELETE"
-    )
-  }
-  next();
+app.get('/people', (req, res) => {
+  db.getUsers(req, res);
 });
-
-app.get('/people', db.getUsers);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
